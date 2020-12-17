576d068da26a395f88c5d970c413dc7d5b36499a
EugeneMorar <melnikov.ev@phystech.edu>
2020-12-18 02:46:11 +0300

    Сделал возможным задание произвольных кнопок управления персонажем.

--- Engine/EntityControllers.py
+++ Engine/EntityControllers.py
@@ -6,6 +6,7 @@ from math import copysign
 import pygame
 
 from .Scene.animations import State
+from .utils.utils import *
 
 ControllerRegistry = {}
 
@@ -49,7 +50,42 @@ class ManualController(EntityController):
     """
     Управление сущностья с клавиатуры
     TODO: разрешить менять игроку взгляд и направление вдоль x во время полёта
+    TODO: разберись в разнице между name и config
     """
+
+    def __init__(self, entity, config='config_wasd.yaml', name='default_contr_name'):
+        super(ManualController, self).__init__(entity, name)
+
+        self.config = config
+
+        self.throw = 0
+        self.hand_hit = 0
+        self.walk_left = 0
+        self.walk_right = 0
+        self.run = 0
+        self.jump = 0
+
+        self.unload_config()
+
+    def unload_config(self):
+        """
+        Принимает yaml файл со списком кнопок в следующем порядке:
+        Бросок,
+        Удар,
+        Идти влево,
+        Идти вправо,
+        Бег,
+        Прыжок
+        :return: list
+        """
+        control_list = load_yaml('src/configs/controllers/' + str(self.config))
+
+        self.throw = control_list[0]
+        self.hand_hit = control_list[1]
+        self.walk_left = control_list[2]
+        self.walk_right = control_list[3]
+        self.run = control_list[4]
+        self.jump = control_list[5]
 
     def step(self, dt):
 
@@ -58,29 +94,29 @@ class ManualController(EntityController):
         velocity = list(self.entity.body.velocity)
 
         # Бросок
-        if pressed_keys[pygame.K_q] and hasattr(self.entity, 'throw'):
+        if pressed_keys[self.throw] and hasattr(self.entity, 'throw'):
             self.entity.throw()
 
-        if pressed_keys[pygame.K_v] and hasattr(self.entity, 'hand_hit'):
+        if pressed_keys[self.hand_hit] and hasattr(self.entity, 'hand_hit'):
             self.entity.hand_hit()
 
         # Если сущность имеет опору под ногами
         if self.entity.can_lean_on_feet():
             # Ходьба
-            if pressed_keys[pygame.K_a] ^ pressed_keys[pygame.K_d]:
+            if pressed_keys[self.walk_left] ^ pressed_keys[self.walk_right]:
                 new_state = State.WALKING
-                if pressed_keys[pygame.K_a]:
+                if pressed_keys[self.walk_left]:
                     velocity[0] = -self.entity.walk_speed
-                elif pressed_keys[pygame.K_d]:
+                elif pressed_keys[self.walk_right]:
                     velocity[0] = self.entity.walk_speed
 
                 # Бег
-                if pressed_keys[pygame.K_LSHIFT]:
+                if pressed_keys[self.run]:
                     velocity[0] = copysign(self.entity.run_speed, velocity[0])
                     new_state = State.RUNNING
 
             # Прыжок
-            if pressed_keys[pygame.K_SPACE]:
+            if pressed_keys[self.jump]:
                 velocity[1] = self.entity.jump_speed
                 # РЫвок, чтобы на следащем шаге проверки не было опоры под ногами
                 self.entity.body.position += (0, 0.05)
